afd57e8f5673cd2546aec725c26666ce0caa9b7a
wanchongyang <chongyang.wan@guanaitong.com>
2018-12-16 22:45:07 +0800

    高可用Spring Cloud Config分布式配置中心

--- springcloud-config-client/src/main/java/com/springcloud/wcy/ConfigClientApplication.java
+++ springcloud-config-client/src/main/java/com/springcloud/wcy/ConfigClientApplication.java
@@ -2,12 +2,18 @@ package com.springcloud.wcy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wanchongyang
  * @date 2018-12-16 22:06
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
--- springcloud-config-server/src/main/java/com/springcloud/wcy/ConfigServerApplication.java
+++ springcloud-config-server/src/main/java/com/springcloud/wcy/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.springcloud.wcy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wanchongyang
@@ -10,6 +12,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
